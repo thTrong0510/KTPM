77c451f801ba6b5208247e34777faab891276d57
thTrong0510 <143087685+thTrong0510@users.noreply.github.com>
2025-03-10 16:37:28 +0700

    Add files via upload

---
+++ db/JdbcUtils.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dht.pojo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author admin
+ */
+public class JdbcUtils {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public static Connection getConn() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost/englishdb", "root", "root");
+    }
+}
